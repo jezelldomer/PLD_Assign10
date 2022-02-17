35b5b7e197fc9756c5452484bdde0ceafb8eee60
jezelldomer <jezelldomer@domain.com>
2022-02-17 22:15:56 +0800

    Importing Modules like webbrowser, cv2 and datetime

---
+++ QRCWebcamReader.py
@@ -0,0 +1,30 @@
+# Assignment 10
+
+# Contact Tracing App
+# 	- Create a python program that will read QRCode using your webcam
+# 	- You may use any online QRCode generator to create QRCode
+# 	- All personal data are in QRCode 
+# 	- You may decide which personal data to include
+# 	- All data read from QRCode should be stored in a text file including the date and time it was read
+
+# Note: 
+# 	- Search how to generate QRCode
+# 	- Search how to read QRCode using webcam
+# 	- Search how to create and write to text file
+# 	- Your source code should be in github before Feb 19
+# 	- Create a demo of your program (1-2 min) and send it directly to my messenger
+
+import webbrowser
+import cv2
+# Installed pip openCv for cv2
+
+import datetime
+import time
+# Imported the datetime and time module for the Real time Management System
+
+def time_(icon):
+    local_time = datetime.now() 
+    datezone = local_time.strftime("%B %d, %Y") 
+    current_time = time.localtime() 
+    timezone = time.strftime("%H:%M", current_time) 
+    timezoneNUM = int(time.strftime("%H", current_time)) 
